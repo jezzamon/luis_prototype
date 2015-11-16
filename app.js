a59e800da916632ec5a80466bc7ac0d4c051dffb
Jerry Relunia <jrelunia@team.com>
2015-11-16 13:57:38 -0500

    first commit

--- app.js
+++ app.js
@@ -5,54 +5,78 @@ $(document).ready(function(){
     $(".pc_first").hover(function(){
         $(".lower_title_text_heading").text("VDI (Virtual Desktop Infrastructure)");    
         $(".lower_title_text").text("Live, continuous and scalable end-to-end intelligence to ensure a smoothly operating VDI environment.");
+        $("#VDI aside").css("color","rgba(255,255,255,1)");
+        $("#VDI img").css("opacity","1");
     },function(){
         $(".lower_title_text_heading").text("Welcome");    
         $(".lower_title_text").text(ipsofacto);
+        $("#VDI aside").css("color","rgba(255,255,255,0.5)");
+        $("#VDI img").css("opacity",".5");
         
     });//end hover (".pc_first")
     
     $(".VI").hover(function(){
         $(".lower_title_text_heading").text("VI(Virtual Infrastructure)"); 
          $(".lower_title_text").text("Live, continuous and scalable cross-silo intelligence to ensure a smoothly operating virtual infrastructure.");
+        $("#VI aside").css("color","rgba(255,255,255,1)");
+        $("#VI img").css("opacity","1");
         },function(){
         $(".lower_title_text_heading").text("Welcome");    
         $(".lower_title_text").text(ipsofacto);
+        $("#VI aside").css("color","rgba(255,255,255,.5)");
+        $("#VI img").css("opacity",".5");    
         
     });//end hover (".VI")
     
     $(".Network").hover(function(){
         $(".lower_title_text_heading").text("Network Suite"); 
          $(".lower_title_text").text("Live, continuous and scalable network intelligence to ensure a smoothly operating virtual and physical network infrastructure.");
+         $("#Network aside").css("color","rgba(255,255,255,1)");
+        $("#Network img").css("opacity","1");
         },function(){
         $(".lower_title_text_heading").text("Welcome");    
         $(".lower_title_text").text(ipsofacto);
+        $("#Network aside").css("color","rgba(255,255,255,.5)");
+        $("#Network img").css("opacity",".5");    
         
     });//end (.Network)
     
     $(".app").hover(function(){
         $(".lower_title_text_heading").text("App Dashboard"); 
          $(".lower_title_text").text("Live, continuous and scalable application delivery intelligence to ensure acceptable end-user experience of applications.");
+        $("#app aside").css("color","rgba(255,255,255,1)");
+        $("#app img").css("opacity","1");
         },function(){
         $(".lower_title_text_heading").text("Welcome");    
         $(".lower_title_text").text(ipsofacto);
+        $("#app aside").css("color","rgba(255,255,255,.5)");
+        $("#app img").css("opacity",".5");
         
     });//end (.mydashboard)
 
     $(".webdash").hover(function(){
         $(".lower_title_text_heading").text("Web Dashboard"); 
          $(".lower_title_text").text("Live, continuous and scalable service delivery intelligence to ensure acceptable end-user experience of web services.");
+         $("#webdash aside").css("color","rgba(255,255,255,1)");
+        $("#webdash img").css("opacity","1");
         },function(){
         $(".lower_title_text_heading").text("Welcome");    
         $(".lower_title_text").text(ipsofacto);
+        $("#webdash aside").css("color","rgba(255,255,255,.5)");
+        $("#webdash img").css("opacity",".5");
          
         });//end (.webdash)
 
      $(".mydash").hover(function(){
         $(".lower_title_text_heading").text("My Dashboard"); 
          $(".lower_title_text").text("Live, continuous and customized dashboard for scoped intelligence about your application, your silo, your department, or your customer.");
-        },function(){
+         $("#mydash aside").css("color","rgba(255,255,255,1)");
+        $("#mydash img").css("opacity","1");
+         },function(){
         $(".lower_title_text_heading").text("Welcome");    
         $(".lower_title_text").text(ipsofacto);
+        $("#mydash aside").css("color","rgba(255,255,255,.5)");
+        $("#mydash img").css("opacity",".5");
          
         });//end (.mydash)
     $(".big_button_wrap").hover(function(){
